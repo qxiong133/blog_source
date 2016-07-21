2fe334a6d3637c820517e31888e4af99cda5d684
codejoy <qxiong133@163.com>
2016-07-21 16:09:04 +0800

    change to local

--- run_hugo.sh
+++ run_hugo.sh
@@ -1,4 +1,4 @@
-hugo server --bind="0.0.0.0" --baseUrl="192.168.33.133:1313" --theme=hyde-y --buildDrafts --watch > /dev/null 2>&1 &
+hugo server --bind="0.0.0.0" --baseUrl="127.0.0.1:1313" --theme=hyde-y --buildDrafts --watch > /dev/null 2>&1 &
 #hugo server --bind="0.0.0.0" --baseUrl="192.168.1.133:1313" --theme=hyde-y --buildDrafts --watch 
 #hugo --theme=hyde --buildDrafts 
 #hugo undraft content/todolist.md
